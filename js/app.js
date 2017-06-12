6ba4e2be465ba9568bfa53ebcef5cb27de4c189f
deanhtid <deanhtid99@gmail.com>
2017-06-12 20:58:35 +0100

    application finished, drawing enabled

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 // Problem: No user interaction causes no change to application.
 // Solution: When user interacts cause changes appropriately.
 
-var color = $('.selected').css('background-color');
+let color = $('.selected').css('background-color');
+let $canvas = $('canvas');
+let context = $canvas[0].getContext('2d');
+let lastEvent;
+let mousedown = false;
 
 // When clicking on control list items
-$('.controls li').click(function() {
+$('.controls').on('click', 'li', function() {
   // Deselect sibling elements
   $(this).siblings().removeClass('selected');
   // Select clicked element
@@ -13,7 +17,7 @@ $('.controls li').click(function() {
   color = $(this).css('background-color');
 });
 
-// When new colour is pressed
+// When 'new colour' is pressed
 $('#revealColorSelect').click(function() {
   // Show or hide the colour select
   changeColor();
@@ -22,18 +26,45 @@ $('#revealColorSelect').click(function() {
 
 // Update the new colour span
 function changeColor() {
-  var r = $('#red').val();
-  var g = $('#green').val();
-  var b = $('#blue').val();
+  let r = $('#red').val();
+  let g = $('#green').val();
+  let b = $('#blue').val();
   $('#newColor').css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
 }
 
 // When color sliders change
 $('input[type=range]').change(changeColor);
 
-// When add colour is pressed
-// Append the colour to the controls ul
-// Select the new color
+// When 'add colour' is pressed
+$('#addNewColor').click(function() {
+  // Append the colour to the controls ul
+  let $newColor = $('<li></li>');
+  $newColor.css('background-color', $('#newColor').css('background-color'));
+  $('.controls ul').append($newColor);
+  // Select the new colour
+  $newColor.click();
+});
 
 // On mouse events on the canvas
-// Draw lines
+$canvas
+  .mousedown(function(e) {
+    lastEvent = e;
+    mousedown = true;
+  })
+  .mousemove(function(e) {
+    // Draw lines
+    if (mousedown) {
+      context.beginPath();
+      context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+      context.lineTo(e.offsetX, e.offsetY);
+      context.strokeStyle = color;
+      context.stroke();
+      lastEvent = e;
+    }
+  })
+  .mouseup(function() {
+    mousedown = false;
+  })
+  .mouseleave(function() {
+    $canvas.mouseup();
+  });
