b38e0c7bdee3599d77acd94e653749d69510d620
deanhtid <deanhtid99@gmail.com>
2017-06-12 19:38:07 +0100

    Added color slider functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 // Problem: No user interaction causes no change to application.
 // Solution: When user interacts cause changes appropriately.
 
+var color = $('.selected').css('background-color');
+
 // When clicking on control list items
-// Deselect sibling elements
-// Select clicked element
+$('.controls li').click(function() {
+  // Deselect sibling elements
+  $(this).siblings().removeClass('selected');
+  // Select clicked element
+  $(this).addClass('selected');
+  // Cache current colour
+  color = $(this).css('background-color');
+});
 
 // When new colour is pressed
-// Show or hide the colour select
+$('#revealColorSelect').click(function() {
+  // Show or hide the colour select
+  changeColor();
+  $('#colorSelect').toggle();
+});
+
+// Update the new colour span
+function changeColor() {
+  var r = $('#red').val();
+  var g = $('#green').val();
+  var b = $('#blue').val();
+  $('#newColor').css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
+}
 
 // When color sliders change
-// Update the new colour span
+$('input[type=range]').change(changeColor);
 
 // When add colour is pressed
 // Append the colour to the controls ul
